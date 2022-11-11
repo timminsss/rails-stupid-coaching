5a3190826fb2bf7a7b706263e2b384478aaa8948
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2022-11-11 12:30:14 +1100

    app now complete

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,17 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @answerArray = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+
+    if params[:question]
+      @question = params[:question]
+      if @question == "I am going to work"
+        @answer = @answerArray[0]
+      elsif @question.include? "?"
+        @answer = @answerArray[1]
+      else
+        @answer = @answerArray[2]
+      end
+    end
   end
 end
