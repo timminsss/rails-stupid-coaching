14070840af45a09c71c7f2a1b85dfb399e5ce1e8
Shelley Timmins <shelley.timmins@hotmail.co.uk>
2022-11-11 12:04:16 +1100

    answer route created

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   # root to: "questions#ask"
-  get "/ask", to: "questions#ask"
+  get '/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
 end
